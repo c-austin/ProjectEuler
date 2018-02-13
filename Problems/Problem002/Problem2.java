4ff9af05b896b745b3814d7dda13617e9386858d
Cory Austin <cory.austin@ryerson.ca>
2018-02-12 20:40:15 -0500

    problem 2

---
+++ Problems/Problem002/Problem2.java
@@ -0,0 +1,22 @@
+public class Problem2{
+  public static void main(String[] args){
+    long limit = 4000000;
+
+    long fibCurr = 2;
+    long fibPrev = 1;
+    long temp = 0;
+
+    long sum = 0;
+
+    while(fibCurr < limit) {
+      if(fibCurr % 2 == 0) {
+        sum += fibCurr;
+      }
+      temp = fibCurr;
+      fibCurr += fibPrev;
+      fibPrev = temp;
+    }
+    
+    System.out.println(sum);
+  }
+}
